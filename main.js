d539b658090396c95f9a164aa5bf998ef361f8a3
Juan David <davidtoloza1234@hotmail.com>
2022-09-29 17:31:50 -0500

    version3 terminado

--- main.js
+++ main.js
@@ -7,24 +7,25 @@ const multi =document.getElementById("multi");
 const division =document.getElementById("division");
 const igual =document.getElementById("igual");
 const borrar =document.getElementById("borrar");
-
+let a=+valor[0].value;
+let b=+valor[1].value;
+let acumular=0
 igual.addEventListener("click",()=>{
-    let a=+valor[0].value;
-    let b=+valor[1].value;
+    b=+display.innerText;
     if(operador.textContent==="+"){
-        display.innerText= b+a;
+        display.innerText= a+b;
         valor[1].value=valor[0].value;
         valor[0].value="";
     }else if(operador.textContent==="-"){
-        display.innerText= b-a;
+        display.innerText= a-b;
         valor[1].value=valor[0].value;
         valor[0].value="";
     }else if(operador.textContent==="*"){
-        display.innerText= b*a;
+        display.innerText= a*b;
         valor[1].value=valor[0].value;
         valor[0].value="";
     }else if(operador.textContent==="/"){
-        display.innerText= b/a;
+        display.innerText= a/b;
         valor[1].value=valor[0].value;
         valor[0].value="";
     }
@@ -32,15 +33,27 @@ igual.addEventListener("click",()=>{
 
 suma.addEventListener("click",()=>{
     operador.textContent= "+";
+    a=+display.innerText;
+    acumular=acumular+a;
+    display.innerText= "";
 })
 resta.addEventListener("click",()=>{
     operador.textContent= "-";
+    a=+display.innerText;
+    acumular=acumular-a;
+    display.innerText= "";
 })
 multi.addEventListener("click",()=>{
     operador.textContent= "*";
+    a=+display.innerText;
+    acumular=acumular*a;
+    display.innerText= "";
 })
 division.addEventListener("click",()=>{
     operador.textContent= "/";
+    a=+display.innerText;
+    acumular=acumular/a;
+    display.innerText= "";
 })
 borrar.addEventListener("click",()=>{
     operador.textContent= "";
@@ -48,3 +61,10 @@ borrar.addEventListener("click",()=>{
     valor[0]="";
     valor[1]="";
 });
+for(let i=0; i<=9; i++){
+    document.getElementById("num"+i).addEventListener("click",presion);
+}
+function presion(e){
+    display.innerText=display.innerText+e.target.value;
+
+}
